9bac593a2aee6d3f19ee08399a0d832d79e159ff
RFoma <mrfattakhov94@gmail.com>
2019-10-25 20:47:38 +0300

    initial commit for the first project

--- Quiz1.py
+++ Quiz1.py
@@ -1,13 +1,26 @@
 import json
-data = '''
-{
-"recipes":
-    {
-      "dish_name": ,
-      " "
-    }
-}
-'''
+d = ['ingredients', 'cooking_steps', 'dish_name']
+data = dict.fromkeays{d}
+
+def recipes_in():
+    print("Из чего будем готовить? (ингредиенты")
+    ingredients = input()
+    print("Расскажи, как нам это делать (шаги)")
+    cooking_steps = input()
+    print("И что в итоге должно получиться? (Назови блюдо)")
+    dish_name = input()
+    #data{"recipes": []}
+    with open("recipes.json", "w"):
+        json.dump(data, s)
+
+def search(x):
+    with open("recipes.json", "r") as f:
+        data = json.loads()
+
+
+def upload_all(data):
+    data = json.loads()
+    for recipes in recipes:
 
 a = True
 while a:
@@ -19,17 +32,16 @@ while a:
 
     if user_input == "Выход":
         break
+
     elif user_input == "Добавить рецепт":
-        print("Из чего будем готовить? (ингредиенты")
-        ingredients = input()
-        print("Расскажи, как нам это делать (шаги)")
-        cooking_steps = input()
-        print("И что в итоге должно получиться? (Назови блюдо)")
-        dish_name = input()
+        recipes_in()
+
     elif user_input == "Найти рецепт по названию":
         print("Какое блюдо Вы хотите найти?")
         name = input()
-        with open("recipes.json", 'r'):
-
+        search()
 
     elif user_input == "Вывести список рецептов":
+        a = upload_all()
+        print(a)
+
