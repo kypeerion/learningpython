d303eb4f3ec48b43d990703526a1656c466b7ceb
kypeerion <kypeerion@gmail.com>
2019-10-27 20:52:55 +0300

    dodelai kolya

--- Test2.py
+++ Test2.py
@@ -8,7 +8,6 @@ data_json= {"Андрей": {"Номер телефона": "89991234567\n",
                                      "Информация":"Падла, мусорнулся.\n"},
             }
 
-
 # def check_file():
 #     try:
 #         check = open('spravochnik.json')
@@ -20,7 +19,7 @@ data_json= {"Андрей": {"Номер телефона": "89991234567\n",
 #             menu_name()
 
 def chtenie_from_file():
-    with open("spravochnik.json", "r", encoding='utf-16') as data_json_r:
+    with open("spravochnik.json", "r") as data_json_r:
         spisok = json.load(data_json_r)
         return spisok
 
@@ -30,11 +29,11 @@ def new_name():
     telephone = input('Введи номер телефона\n')
     info = input('Введи информацию о контакте\n')
     zapis = {"Номер телефона": "{0}".format(telephone),
-                          "Информация":"{0}".format(info)
-                     }
-    zapis_v_file(name,zapis)
-    with open("spravochnik.json", "a", encoding='utf-16') as data_json_a:
+                          "Информация":"{0}".format(info)}
+    zapis_v_file = {name:zapis}
+    with open("spravochnik.json", "a") as data_json_a:
         json.dump(zapis_v_file, data_json_a)
+    print("Записал тебе в ебало, проверяй")
 
 def poisk_po_name():
     # with open("spravochnik.json", "r", encoding='utf-16') as data_json_r:
@@ -43,8 +42,7 @@ def poisk_po_name():
     #     for i in spisok:
     #     if i[''] == vvod:
     #         print(i)
-    with open("spravochnik.json", "r", encoding='utf-16') as spisok:
-    while True:
+    with open("spravochnik.json", "r") as spisok:
         vvod = input('Введи имя:')
         spisok_naidenogo = ''
         chislo_spiska = 1
@@ -67,8 +65,8 @@ def poisk_po_name():
 
 
 def menu_name():
-    print('1) Найти запись\n2) Добавить запись\n3) Показать все записи\n4) Выход')
     while True:
+        print('1) Найти запись\n2) Добавить запись\n3) Показать все записи\n4) Выход')
         vvod = input('Выбери нужный пункт меню: \n')
         if vvod == '1':
             poisk_po_name()
@@ -92,7 +90,7 @@ def pokazat_all_spisok():
     # spisok_zapisey = []
     # for zapis, chislo in zip(spisok, range(chislo_zapisey)):
     #     spisok_zapisey.append('{0}) {1}'.format(chislo+1, zapis))
-    with open("spravochnik.json", "r", encoding='utf-32') as data_json_r:
+    with open("spravochnik.json", "r") as data_json_r:
         spisok = json.load(data_json_r)
         print (spisok)
     vvod = input('\nДля возврата в меню, напиши "Да"\n')
